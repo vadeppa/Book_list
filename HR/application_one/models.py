c14ae58d5d8ecbd1777af6a52a72b017beeafe75
Ravindrareddygontu <ravindrareddy72868@gmail.com>
2024-08-12 12:58:33 +0530

    added bookl ist

---
+++ HR/application_one/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+# books/models.py
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
---
+++ HR/application_one/views.py
@@ -0,0 +1,8 @@
+
+
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
